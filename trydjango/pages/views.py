a188d01bc5b1cf31bdabc1dc406b2da51f0ee242
Rory Murray <rory.christian.murray@gmail.com>
2019-11-16 15:18:00 -0500

    Create home template

--- trydjango/pages/views.py
+++ trydjango/pages/views.py
@@ -4,13 +4,18 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")  # string of html code
+def home_view(request, *args, **kwargs):
+    # print(args, kwargs)
+    print(request.user)
+    # return HttpResponse("<h1>Hello World</h1>")  # string of html code
+    return render(request, "home.html", {})
 
 
-def contact_view(*args, **kwargs):
+def contact_view(request, *args, **kwargs):
+    print(request.user)
     return HttpResponse("<h1>Contact Page</h1>")
 
 
-def about_view(*args, **kwargs):
+def about_view(request, *args, **kwargs):
+    print(request)
     return HttpResponse("<h1>Rory... he's a cool guy, I've heard")
