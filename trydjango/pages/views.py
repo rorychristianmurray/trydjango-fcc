23e59bdc1b57078343e37c7ee722b322ea041299
Rory Murray <rory.christian.murray@gmail.com>
2019-11-16 15:07:20 -0500

    Create contact and about page

--- trydjango/pages/views.py
+++ trydjango/pages/views.py
@@ -6,3 +6,11 @@ from django.shortcuts import render
 
 def home_view(*args, **kwargs):
     return HttpResponse("<h1>Hello World</h1>")  # string of html code
+
+
+def contact_view(*args, **kwargs):
+    return HttpResponse("<h1>Contact Page</h1>")
+
+
+def about_view(*args, **kwargs):
+    return HttpResponse("<h1>Rory... he's a cool guy, I've heard")
